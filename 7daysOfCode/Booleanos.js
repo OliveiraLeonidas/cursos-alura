329859a88da98a6569a7b41841a6619f434d663c
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-16 14:02:42 -0300

    booleano

---
+++ 7daysOfCode/Booleanos.js
@@ -0,0 +1,16 @@
+//day one - 16/02/2023
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+const comparador = (numero, string) => 
+(numero === string) ? console.log("As variáveis tem o mesmo valor, mas tipos diferentes") :console.log('As variáveis tem tipos diferentes')
+
+comparador(numeroUm, stringUm)
+comparador(numeroTrinta, stringTrinta)
+comparador(numeroDez, stringDez)
+
+
