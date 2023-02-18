14a98443337b847bb79a8e0fbdd153982d933d7a
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-18 19:19:54 -0300

    versao2

--- 7daysOfCode/codeDay2/main.js
+++ 7daysOfCode/codeDay2/main.js
@@ -6,24 +6,28 @@ function click() {
     
     let botaoEnviar = document.getElementById("btn");
     let mostraResposta = document.querySelector("#resposta");
-
+    let mensagemFinal = document.querySelector(".container__formulario");
+    
     botaoEnviar.onclick = function() {
-        mostraResposta.innerHTML = '<p>' + `Óla, ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${programacao.value}` +'</p>';
+        mostraResposta.innerHTML = '<p>' + `Óla ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${programacao.value}.` + '</p>';
         
-        setTimeout(novaPergunta, 2000)
+        setTimeout(novaPergunta,1000);
     };
     
-
+    
     function novaPergunta() {
-        let mensagemFinal = document.querySelector(".container__formulario");
-        let resposta = prompt(`Você está gostando de estudar ${programacao.value}, responda com 1 para SIM, e 2 para NÃO.`);
+        mensagemFinal.innerHTML = '<div id="blocoResposta2" >' + `Você está gostando de estudar ${programacao.value}, responda com 1 para SIM, e 2 para NÃO.`+ '<input type="number" id="inputResposta">' + '<button id="btn2" class="container__formulario__botao">' + 'enviar' + '</button>' + '</div>'
+        
+        let botaoEnviar2 = document.getElementById("btn2");
+        let resposta = document.querySelector('#inputResposta');
 
-
-        if (resposta == 1) {
-            mensagemFinal.innerHTML = '<div>' + ` Muito bom! Continue estudando e você terá muito sucesso. ` + '</div>'
-            
-        }else if (resposta == 2) {
-            mensagemFinal.innerHTML = '<div>' + ` Ahh que pena... Já tentou aprender outras linguagens? ` + '</div>'
+        botaoEnviar2.onclick = function (){
+            if (resposta.value == 1) {
+                mensagemFinal.innerHTML = '<div class="feedback">' + ` Muito bom! Continue estudando e você terá muito sucesso. ` + '</div>'
+               
+           }else if (resposta.value == 2) {
+                mensagemFinal.innerHTML = '<div class="feedback">' + ` Ahh que pena... Já tentou aprender outras linguagens? ` + '</div>'
+           };
         };
     };
     
