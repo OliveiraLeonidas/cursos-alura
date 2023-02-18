af10c58898281ad406485f9cbe3afe72c19039df
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-18 12:14:04 -0300

    codeDay2

--- 7daysOfCode/codeDay2/main.js
+++ 7daysOfCode/codeDay2/main.js
@@ -8,41 +8,23 @@ function click() {
     let mostraResposta = document.querySelector("#resposta");
 
     botaoEnviar.onclick = function() {
-        
         mostraResposta.innerHTML = '<p>' + `Óla, ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${programacao.value}` +'</p>';
-
         
-        
-        setTimeout(novaPergunta, 10000)
-
-        //nome.value = ""; idade.value = ""; programacao.value = "";
-
+        setTimeout(novaPergunta, 2000)
     };
     
 
     function novaPergunta() {
-        
-        let pergunta = document.querySelector(".container__formulario");
         let mensagemFinal = document.querySelector(".container__formulario");
-        
-        // pergunta.innerHTML = '<div>' + `Você está gostando de estudar ${programacao.value}, responda com 1 para SIM, e 2 para NÃO.`
-        
-        let resposta = prompt(`Você está gostando de estudar ${programacao.value}, responda com 1 para SIM, e 2 para NÃO.`)
-        
-        console.log(resposta)
-        //let respostaValor = document.querySelector('input[type="number"]');
+        let resposta = prompt(`Você está gostando de estudar ${programacao.value}, responda com 1 para SIM, e 2 para NÃO.`);
+
 
         if (resposta == 1) {
             mensagemFinal.innerHTML = '<div>' + ` Muito bom! Continue estudando e você terá muito sucesso. ` + '</div>'
-            console.log("Muito bom! Continue estudando e você terá muito sucesso.")
+            
         }else if (resposta == 2) {
             mensagemFinal.innerHTML = '<div>' + ` Ahh que pena... Já tentou aprender outras linguagens? ` + '</div>'
-            console.log("Ahh que pena... Já tentou aprender outras linguagens?")
-        }
-    }
+        };
+    };
     
-}
-
-
-
-click();
+}; click();
