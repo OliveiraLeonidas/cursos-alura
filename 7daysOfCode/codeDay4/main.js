f33522ef69ce3359b83c596fd0a1970cdcded1ce
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-20 14:52:23 -0300

    pageReload

--- 7daysOfCode/codeDay4/main.js
+++ 7daysOfCode/codeDay4/main.js
@@ -1,12 +1,9 @@
 let testar = document.querySelector('#btnTestar');
 let btnStart = document.querySelector('#btnTestar');
-let numSecreto =Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let numSecreto = 7
 let feedback = document.querySelector('.container__box__jogo');
 let cont = 2;
 
-
-
-//btnStart.onclick = start;
 
 btnStart.onclick = start
 function start() {    
@@ -14,18 +11,24 @@ function start() {
         let tentativa = prompt(`Digite a sua ${i+1}ª tentativa`);
         if(tentativa == numSecreto){
             feedback.innerHTML += '<div>' + `Você acertou, o número é ${numSecreto}` + '</div>';
-
+            replay()
             break;
         }else if (i == 2) {
             feedback.innerHTML += '<div>' + `Você infelizmente não conseguiu dessa vez.` + '</div>';
-
+            replay()
+            break
         }else if (tentativa != numSecreto){
             alert(`Você errou, tente mais ${cont--}`);
-
-
+            replay()
+            break
         }
     }
+
 }
 
-
+function replay() {
+    feedback.innerHTML += '<button id="btnReplay">' + `Jogar novamente` + '</button>';
+    let btnReplay = document.querySelector('#btnReplay');
+    btnReplay.onclick = () => window.location.reload(true);
+}
 
