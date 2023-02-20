b8523c1a8885a41ef984abb3fe625f3f13ff5b4b
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-20 14:31:47 -0300

    adicionandobtn

--- 7daysOfCode/codeDay4/main.js
+++ 7daysOfCode/codeDay4/main.js
@@ -1,24 +1,31 @@
 let testar = document.querySelector('#btnTestar');
-let numSecreto =Math.floor(Math.random() * (10 - 0 + 1) + 0)
-let feedback = document.querySelector('.container__box__jogo')
+let btnStart = document.querySelector('#btnTestar');
+let numSecreto =Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let feedback = document.querySelector('.container__box__jogo');
 let cont = 2;
 
 
-for (let i =0; i<=2;i++){
-    let tentativa = prompt(`Digite a sua ${i+1}ª tentativa`)
-    if(tentativa == numSecreto){
-        feedback.innerHTML += '<div>' + `Você acertou, o número é ${numSecreto}` + '</div>';
-        break
-    }else if (i == 2) {
-        feedback.innerHTML += '<div>' + `Você infelizmente não conseguiu dessa vez.` + '</div>';
-    }else if (tentativa != numSecreto){
-        alert(`Você errou, tente mais ${cont--}`)
 
-    } 
+//btnStart.onclick = start;
+
+btnStart.onclick = start
+function start() {    
+    for (let i =0; i<=2;i++){
+        let tentativa = prompt(`Digite a sua ${i+1}ª tentativa`);
+        if(tentativa == numSecreto){
+            feedback.innerHTML += '<div>' + `Você acertou, o número é ${numSecreto}` + '</div>';
+
+            break;
+        }else if (i == 2) {
+            feedback.innerHTML += '<div>' + `Você infelizmente não conseguiu dessa vez.` + '</div>';
+
+        }else if (tentativa != numSecreto){
+            alert(`Você errou, tente mais ${cont--}`);
+
+
+        }
+    }
 }
-
-
-
 
 
 
