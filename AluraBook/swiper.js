259b50119c63378e274c724a7b6e6738d41ca981
Leonidas Oliveira <leonidasoliv25@gmail.com>
2023-02-27 15:48:53 -0300

    iniciando-responsividade

---
+++ AluraBook/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 3,
+    spaceBetween: 15, 
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        }
+    }
+);
